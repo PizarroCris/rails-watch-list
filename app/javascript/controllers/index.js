b1f511394829940cf09836fbc3d5615911017f50
Cristiano Pizarro <dev.cristianopizarro@gmail.com>
2025-08-30 02:08:56 +0100

    Add dotenv-rails

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,14 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("upload-image");
+  const fileName = document.getElementById("file-name");
+
+  if (input) {
+    input.addEventListener("change", () => {
+      fileName.textContent = input.files.length > 0 ? input.files[0].name : "No file selected";
+    });
+  }
+});
